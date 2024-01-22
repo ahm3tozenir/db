e4f05c9681ef92456da57e577ed8041457df16ef
Ahmet Özenir <101597537+ahm3tozenir@users.noreply.github.com>
2024-01-22 17:47:31 +0300

    Update Program.cs

--- DataBaseExample/Program.cs
+++ DataBaseExample/Program.cs
@@ -1,3 +1,15 @@
 // See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
+using DatabaseExample.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+
+
+ExampleDbContext db = new();
+db.Users.Add(new DatabaseExample.Entities.User { FirstName = "a", lastNam = "s", IdentificationNumber = "1231241", IsActive = true, Password = "1234", UserName = "asd123" });
+db.Jobbers.Add(new DatabaseExample.Entities.Jobber { })
+db.SaveChanges();e
+var user = db.Users.FirstOrDefault();
+Console.WriteLine(JsonConvert.SerializeObject(user));
+Console.ReadLine();
